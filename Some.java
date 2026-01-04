21b5c15848fa060c98df8d2bb0d11ea0eab2fd79
Guru-1205 <premguru1045@gmail.com>
2026-01-04 21:14:45 +0530

    Update string initialization in Main.java and add Some.java with a basic structure

---
+++ Some.java
@@ -0,0 +1,12 @@
+public class Some {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+// team - multiple developers - there is a dev called X -> feature A -> featureADevBranch from main
+// team lead -> X -> urgent bug fix task -> bugFix111
+// u have some untracked changes in a branch -> vera branch move trying -> 
+// option 1 -> changes from src branch will be migrated to newly created branch
+// option 2 -> changes from src branch will be deleted
+// option 3 -> stash - saving the changes locally
+// option 4 -> changes ellam commit pannanum
